a03325fd1cef1dcab777b8c605edf1d708be597d
raginisilimkar <raginisilimkar3@gmail.com>
2025-08-06 11:38:18 +0530

    Fix Vercel blank page config

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL
   
 });
 console.log("API URL:", import.meta.env.VITE_API_URL);
